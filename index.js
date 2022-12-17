f0198a121b143205b2e100c7682dc64b560d7c59
shihabcse1 <tomdshihab@gmail.com>
2022-12-18 00:42:40 +0600

    post api added

--- index.js
+++ index.js
@@ -2,41 +2,50 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mysql = require("./connection").con;
+const cors = require("cors"); // importing middleware
 
-// Routing
+app.use(cors());
+app.use(express.json());
+
+const generate_key = function () {
+    return Math.floor(Math.random() * 1000 + 1);
+};
+
 app.get("/", (req, res) => {
-    res.send("Server is running");
-});
-
-app.get("/add", (req, res) => {
     // fetching data from form
     const { sessionId } = req.query;
 
     // Sanitization XSS...
-    let qry = "select * from time_tracker where session_id=?";
-    mysql.query(qry, [sessionId], (err, results) => {
+    let qry = "select * from time_tracker";
+    mysql.query(qry, (err, results) => {
         if (err) throw err;
         else {
-            if (results.length > 0) {
-                //res.render("add", { checkmesg: true });
-                res.send(results);
-            } else {
-                // insert query
-                let qry2 = "insert into time_tracker values(?,?,?,?)";
-                mysql.query(
-                    qry2,
-                    //["03", "22/03/22", 2000, 2],
-                    [sessionId, date, totalTime, totalPause],
-                    (err, results) => {
-                        if (results.affectedRows > 0) {
-                            //res.render("add", { mesg: true });
-                            res.send(results);
-                        }
-                    }
-                );
-            }
+            res.send(results);
         }
     });
+});
+
+app.post("/", (req, res) => {
+    // fetching data from form
+
+    console.log("This is body", req.body);
+
+    //res.send(req.body);
+
+    const { session_id, date, total_time, total_pause } = req.body;
+
+    // Sanitization XSS...
+    let qry2 = "insert into time_tracker values(?,?,?,?)";
+    mysql.query(
+        qry2,
+        [generate_key(), total_time, total_pause, date],
+        (err, results) => {
+            if (results.affectedRows > 0) {
+                //res.render("add", { mesg: true });
+                res.send(results);
+            }
+        }
+    );
 });
 
 //Create Server
